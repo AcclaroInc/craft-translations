9cd19d1e459059a92468d08d60f3963f86a17d08
sidedwards <edwards.sid@gmail.com>
2019-06-14 16:39:54 -0600

    :bug: Update XML original url to use public url if entry is live

--- src/services/UrlGenerator.php
+++ src/services/UrlGenerator.php
@@ -106,8 +106,8 @@ class UrlGenerator
         // If we're looking at the live version of an entry, just use
         // the entry's main URL as its share URL
         if ($className === Entry::class && $element->getStatus() === Entry::STATUS_LIVE) {
-            $variables['shareUrl'] = $element->getUrl();
-            $previewUrl = Translations::$plugin->urlHelper->actionUrl('entries/share-entry', $params);
+            // $variables['shareUrl'] = $element->getUrl();
+            $previewUrl = $element->getUrl();
         } else {
             switch ($className) {
                 case EntryDraft::class:
