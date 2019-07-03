890a3d1a2202b8e45ca616e0fc0c45f994a43f2a
shnsumit <shnsumit@gmail.com>
2019-07-04 01:01:38 +0530

    Task Batching - order submission

--- src/controllers/BaseController.php
+++ src/controllers/BaseController.php
@@ -1023,99 +1023,11 @@ class BaseController extends Controller
 
         if (Craft::$app->getRequest()->getParam('submit')) {
 
-            // Check supported languages
-            if ($order->getTranslator()->service !== 'export_import') {
-                $translationService = Translations::$plugin->translationFactory->makeTranslationService($order->getTranslator()->service, $order->getTranslator()->getSettings());
-            
-                if ($translationService->getLanguages()) {
-                    $sourceLanguage = Translations::$plugin->siteRepository->normalizeLanguage(Craft::$app->getSites()->getSiteById($order->sourceSite)->language);
-                    $unsupported = false;
-                    $unsupportedLangs = [];
-                    $supportedLanguagePairs = [];
-
-                    foreach ($translationService->getLanguagePairs($sourceLanguage) as $key => $languagePair) {
-                        $supportedLanguagePairs[] = $languagePair->target['code'];
-                    }
-
-                    foreach (json_decode($order->targetSites) as $key => $siteId) {
-                        $site = Craft::$app->getSites()->getSiteById($siteId);
-                        $language = Translations::$plugin->siteRepository->normalizeLanguage($site->language);
-                        
-                        if (!in_array($language, array_column($translationService->getLanguages(), 'code'))) {
-                            $unsupported = true;
-                            $unsupportedLangs[] = array(
-                                'language' => $site->name .' ('.$site->language.')'
-                            );
-                        }
-                        
-                        if (!in_array($language, $supportedLanguagePairs)) {
-                            $unsupported = true;
-                            $unsupportedLangs[] = array(
-                                'language' => $site->name .' ('.$site->language.')'
-                            );
-                        }
-                    }
-
-                    if ($unsupported || !empty($unsupportedLangs)) {
-                        $order->status = 'failed';
-                        $success = Craft::$app->getElements()->saveElement($order);
-                        if (!$success) {
-                            Craft::error('Couldn’t save the order', __METHOD__);
-                        }
-                        Craft::$app->getSession()->setError(Translations::$plugin->translator->translate('app', 'The following language pair(s) are not supported: '.implode(', ', array_column($unsupportedLangs, 'language')).' Contact Acclaro for assistance.'));
-                        // return; // @todo This might be a better idea than failing the order
-                        return $this->redirect('translations/orders', 302, true);
-                    }
-
-                } else {
-                    // var_dump('Could not fetch languages');
-                }
-            }
-
-            $order->logActivity(sprintf(Translations::$plugin->translator->translate('app', 'Order Submitted to %s'), $order->translator->getName()));
-
-            $drafts = Translations::$plugin->jobFactory->dispatchJob(CreateOrderTranslationDrafts::class, $order->getTargetSitesArray(), $order->getElements(), $order->title);
-
-            foreach ($drafts as $draft) {
-                $file = Translations::$plugin->fileRepository->makeNewFile();
-                $element = Craft::$app->getElements()->getElementById($draft->id, null, $order->sourceSite);
-                
-                if ($draft instanceof GlobalSet) {
-                    $targetSite = $draft->site;
-                } else {
-                    $targetSite = $draft->siteId;
-                }
-
-                $file->orderId = $order->id;
-                $file->elementId = $draft->id;
-                $file->draftId = $draft->draftId;
-                $file->sourceSite = $order->sourceSite;
-                $file->targetSite = $targetSite;
-                $file->previewUrl = Translations::$plugin->urlGenerator->generateElementPreviewUrl($draft, $targetSite);
-                $file->source = Translations::$plugin->elementToXmlConverter->toXml(
-                    $element,
-                    $draft->draftId,
-                    $order->sourceSite,
-                    $targetSite,
-                    $file->previewUrl
-                );
-                $file->wordCount = isset($wordCounts[$draft->id]) ? $wordCounts[$draft->id] : 0;
-
-                Translations::$plugin->fileRepository->saveFile($file);
-            }
-
-            // Only send order to translation service when not Manual
-            if ($order->translator->service !== 'export_import') {
-                Translations::$plugin->jobFactory->dispatchJob(SendOrderToTranslationService::class, $order);
-            } else {
-                $order->status = 'in progress';
-                $order->dateOrdered = new DateTime();
-
-                $success = Craft::$app->getElements()->saveElement($order);
-                if (!$success) {
-                    Craft::error('Couldn’t save the order', __METHOD__);
-                }
-            }
+            Craft::$app->queue->push(new OrderJob([
+                'description' => 'Order submitted',
+                'orderId' => $order->getId(),
+                'wordCounts' => $wordCounts,
+            ]));
 
             Craft::$app->getSession()->setNotice(Translations::$plugin->translator->translate('app', 'Order Submitted.'));
         } else {
