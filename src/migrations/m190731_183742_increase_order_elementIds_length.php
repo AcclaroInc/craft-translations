cb3ee9d470cf960a8cb3c9413c32a3cdd1b682c3
sidedwards <edwards.sid@gmail.com>
2019-07-31 14:14:22 -0600

    :card_file_box: Increased elementIds char limit

---
+++ src/migrations/m190731_183742_increase_order_elementIds_length.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace acclaro\translations\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m190731_183742_increase_order_elementIds_length migration.
+ */
+class m190731_183742_increase_order_elementIds_length extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        // Place migration code here...
+        echo "Altering translations_orders elementIds column...\n";
+        $this->alterColumn('{{%translations_orders}}', 'elementIds', $this->string(8160)->notNull()->defaultValue(''));
+        echo "Done altering translations_orders elementIds column...\n";
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m190731_183742_increase_order_elementIds_length cannot be reverted.\n";
+        return false;
+    }
+}
+
