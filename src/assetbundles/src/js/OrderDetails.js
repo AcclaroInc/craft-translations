684f6e09f6cbee4ec5ec6560ecbcb7843827e79d
Sid Edwards <sid@semantic.dev>
2021-09-22 15:10:26 -0600

    fix: enable order settings tab for local orders
    - updated the tab title text

--- src/assetbundles/src/js/OrderDetails.js
+++ src/assetbundles/src/js/OrderDetails.js
@@ -894,14 +894,11 @@
         },
         _disableOrderSettingsTab: function() {
             var $proceed = false;
-            if (isDefaultTranslator && isPublished) {
-                $proceed = true;
-            }
             if (!isDefaultTranslator && (isCompleted || isPublished || isCanceled)) {
                 $proceed = true;
             }
             if ($proceed) {
-                $('li[data-id=order]').attr('title', 'This order is no longer editable. The corresponding order is '+$("#order-attr").data("status"));
+                $('li[data-id=order]').attr('title', 'This order is no longer editable. The corresponding My Acclaro order is '+$("#order-attr").data("status")+'.');
                 $('#tab-order').addClass('noClick');
             }
         }
