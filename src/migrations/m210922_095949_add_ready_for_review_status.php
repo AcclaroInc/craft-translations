ba0ec5cdbbb269962d3ac8c3be54fc9a7b3286cb
Sumit Sharma <shnsumit@gmail.com>
2021-10-09 03:27:44 +0530

    Remving enum constraints before altering enum status column

--- src/migrations/m210922_095949_add_ready_for_review_status.php
+++ src/migrations/m210922_095949_add_ready_for_review_status.php
@@ -2,9 +2,10 @@
 
 namespace acclaro\translations\migrations;
 
-use acclaro\translations\Constants;
 use Craft;
+use craft\helpers\App;
 use craft\db\Migration;
+use acclaro\translations\Constants;
 
 /**
  * m210922_095949_add_ready_for_review_status migration.
@@ -16,12 +17,18 @@ class m210922_095949_add_ready_for_review_status extends Migration
      */
     public function safeUp()
     {
+        if (App::env('DB_DRIVER') == 'pgsql') {
+            echo "Dropping existing constraints...\n";
+            $this->dropConstraint('translations_files_status_check', 'translations_files');
+            $this->dropConstraint('translations_orders_status_check', 'translations_orders');
+        }
+
         echo "Altering translations_files status column...\n";
-        $this->alterColumn('{{%translations_files}}', 'status', $this->enum('status', Constants::FILE_STATUSES)->defaultValue('new'));
+        $this->alterColumn('{{%translations_files}}', 'status', $this->enum('status', Constants::FILE_STATUSES)->notNull()->defaultValue('new'));
         echo "Done altering translations_files status column...\n";
 
         echo "Adding translations_orders status column...\n";
-        $this->alterColumn('{{%translations_orders}}', 'status', $this->enum('status', Constants::ORDER_STATUSES)->defaultValue('new'));
+        $this->alterColumn('{{%translations_orders}}', 'status', $this->enum('status', Constants::ORDER_STATUSES)->notNull()->defaultValue('new'));
         echo "Done adding translations_orders status column...\n";
 
         echo "Altering translations_orders activity log column...\n";
@@ -36,5 +43,10 @@ class m210922_095949_add_ready_for_review_status extends Migration
     {
         echo "m210922_095949_add_ready_for_review_status cannot be reverted.\n";
         return false;
+    }
+
+    private function dropConstraint($contraint, $table)
+    {
+        Craft::$app->getDb()->createCommand("ALTER TABLE {{%$table}} DROP CONSTRAINT IF EXISTS $contraint")->execute();
     }
 }
