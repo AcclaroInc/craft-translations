15efa13fdfb8bdcebf599958ce0453e9bb1fd7a3
Sumit Sharma <shnsumit@gmail.com>
2022-02-17 03:05:56 +0530

    fix: missing title on merge into draft

--- src/Translations.php
+++ src/Translations.php
@@ -616,22 +616,34 @@ class Translations extends Plugin
     private function _onDeleteElement(Event $event)
     {
         if (!empty($event->element->draftId)) {
-            $response = Translations::$plugin->draftRepository->isTranslationDraft($event->element->draftId);
+			$response = self::$plugin->draftRepository->isTranslationDraft($event->element->draftId);
             if ($response) {
 
                 $currentFile = self::$plugin->fileRepository->getFileByDraftId($event->element->draftId);
 
                 if ($currentFile) {
-                    $order = self::$plugin->orderRepository->getOrderById($currentFile->orderId);
+					$currentFile->status 		= Constants::FILE_STATUS_CANCELED;
+					$currentFile->draftId 		= null;
+					$currentFile->target 		= null;
+					$currentFile->previewUrl	= null;
+
+					self::$plugin->fileRepository->saveFile($currentFile);
+
+					$order = self::$plugin->orderRepository->getOrderById($currentFile->orderId);
 
                     if ($order) {
-                        $order->logActivity(Translations::$plugin->translator->translate('app', 'Draft '. $event->element->draftId .' deleted.'));
-                        Translations::$plugin->orderRepository->saveOrder($order);
-                    }
+						$order->logActivity(self::$plugin->translator->translate('app', 'Draft ' . $event->element->draftId . ' deleted.'));
+						// Update order with new status
+						$newStatus = self::$plugin->orderRepository->getNewStatus($order);
 
-                    $currentFile->status = Constants::FILE_STATUS_CANCELED;
+						if ($order->status !== $newStatus) {
+							$order->status = $newStatus;
+							self::$plugin->orderRepository->saveOrder($order);
 
-                    self::$plugin->fileRepository->saveFile($currentFile);
+							$order->logActivity(sprintf('Order status changed to \'%s\'', $order->getStatusLabel($newStatus)));
+						}
+						self::$plugin->orderRepository->saveOrder($order);
+					}
                 }
             }
         }
@@ -641,7 +653,7 @@ class Translations extends Plugin
             $event->hardDelete = true;
         }
 
-        if ($order = Translations::$plugin->orderRepository->isTranslationOrder($event->element->id) && $event->hardDelete) {
+		if ($order = self::$plugin->orderRepository->isTranslationOrder($event->element->id) && $event->hardDelete) {
             $drafts = [];
             /** @var Order|null $order */
             foreach ($order->getFiles() as $file) {
