cbea308166ee7b37278ed21d5db1a5ab19efa880
Bhupesh Pandey <bhupesh@appfoster.com>
2023-07-03 13:05:25 +0530

    Fixed: draft creation issue

--- src/services/repository/EntryRepository.php
+++ src/services/repository/EntryRepository.php
@@ -69,10 +69,12 @@ class EntryRepository extends Component
 
 		$transaction = Craft::$app->getDb()->beginTransaction();
         try {
+            $targetSiteId = $newAttributes['siteId'];
+            $enabledForSites = Craft::$app->getElements()->getEnabledSiteIdsForElement($canonical->id);
+            $isNewForSite = !in_array($targetSiteId, $enabledForSites);
+
             // Create the draft row
             $draftId = (new Drafts())->insertDraftRow($name, $notes, $creatorId, $canonical->id, $canonical::trackChanges(), $provisional);
-            $targetSiteId = $newAttributes['siteId'];
-            unset($newAttributes['siteId']);
             // Duplicate the element
             $newAttributes['isProvisionalDraft'] = $provisional;
             $newAttributes['canonicalId'] = $canonical->id;
@@ -85,13 +87,19 @@ class EntryRepository extends Component
                 'trackChanges' => $canonical::trackChanges(),
             ];
 
+            if ($isNewForSite) {
+                unset($newAttributes['siteId']);
+            }
             $draft = Craft::$app->getElements()->duplicateElement($canonical, $newAttributes);
 
-            $draft->siteId = $targetSiteId;
-            if (!Craft::$app->getElements()->saveElement($draft)) {
-                throw new \Exception("Failed to save draft in target site");
+            if ($isNewForSite) {
+                // We can only set the target site it it does not exist else craft erros out.
+                $draft->siteId = $targetSiteId;
+                if (!Craft::$app->getElements()->saveElement($draft)) {
+                    throw new \Exception("Failed to save draft in target site");
+                }
+                $draft = Craft::$app->getElements()->propagateElement($draft, $targetSiteId);
             }
-            $draft = Craft::$app->getElements()->propagateElement($draft, $targetSiteId);
 
             $transaction->commit();
         } catch (\Throwable $e) {
