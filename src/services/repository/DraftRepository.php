92cfe947272addeca3f3ee0ba58726ac4cab23a2
Bhupesh Pandey <bhupesh@appfoster.com>
2023-11-17 20:07:51 +0530

    fixed: supertable block duplication

--- src/services/repository/DraftRepository.php
+++ src/services/repository/DraftRepository.php
@@ -266,20 +266,7 @@ class DraftRepository
                     $draft = Translations::$plugin->assetDraftRepository->createDraft($element, $site, $order->title, $order->sourceSite);
                     break;
                 default:
-                    // if ($isDraft) {
-                        $draft = Translations::$plugin->entryRepository->createDraft($element, $site, $order->title);
-                    // } else {
-                    //     $creatorId = Craft::$app->getUser()->getId();
-                    //     $allSitesHandle = Translations::$plugin->siteRepository->getAllSitesHandle();
-                    //     $handle = isset($allSitesHandle[$site]) ? $allSitesHandle[$site] : "";
-                    //     $name = sprintf('%s [%s]', $order->title, $handle);
-                    //     $elementURI = Craft::$app->getElements()->getElementUriForSite($element->id, $site);
-                    //     $attributes = [
-                    //         'siteId' => $site,
-                    //         'uri' => $elementURI
-                    //     ];
-                    //     $draft = Craft::$app->getDrafts()->createDraft($element, $creatorId, $name, null, $attributes);
-                    // }
+                    $draft = Translations::$plugin->entryRepository->createDraft($element, $site, $order->title);
             }
         } catch(Exception $e) {
             throw $e;
