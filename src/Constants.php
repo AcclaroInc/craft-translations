98ccb3a2980ad693031795cc7c03d0eaa6a4a5e8
Sid Edwards <edwards.sid@gmail.com>
2024-11-08 13:33:39 -0700

    Update CTA links in Constants.php
    - Changed `CTA_PROFESSIONAL_TRANSLATION` constant to a new URL

--- src/Constants.php
+++ src/Constants.php
@@ -324,7 +324,7 @@ class Constants
         'craft\fields\Money',
     ];
 
-    const CTA_PROFESSIONAL_TRANSLATION = "https://www.acclaro.com/solutions/global-marketing/website-localization-services";
+    const CTA_PROFESSIONAL_TRANSLATION = "https://www.acclaro.com/get-started/translation-quote/?utm_campaign=Craft&utm_source=craft_services&utm_medium=get_quote_button";
     const CTA_QUALITY_REVIEW = "https://www.acclaro.com/solutions/language-quality";
     const CTA_TECHNICAL_SUPPORT = "https://info.acclaro.com/craftcms-technical-support";
     const CTA_AI_ML_TRANSLATION = "#";
